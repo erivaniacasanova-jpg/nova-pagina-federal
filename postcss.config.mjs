127481378399affc5b770268e85000a56b1e4f8e
erivaniacasanova-jpg <erivaniacasanova@gmail.com>
2025-11-24 17:28:37 -0300

    Add files via upload

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,3 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    '@tailwindcss/postcss': {},
-  },
+export default {
+  plugins: {}
 }
-
-export default config
